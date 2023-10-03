aad878e4aba7eecf807bf29730de6fd499e546ca
Kanno Beck <drkannobeck@gmail.com>
2023-10-03 14:38:17 +0100

    update

--- src/components/faq/Faq.jsx
+++ src/components/faq/Faq.jsx
@@ -5,7 +5,7 @@ const Faq = () => {
     <div className="px-4 relative max-w-6xl mx-auto pt-4 pb-16 md:py-12 xl:py-32">
 
       <div>
-        <div className="pb-4">
+        <div className="py-4 sm:pt-0">
           <h1 className="text-2xl font-display text-primary md:text-5xl font-semibold pb-4">Who is Portal Check for?</h1>
         </div>
         <QnA />
